866e2798860b34127a3948eeff9d2623fdd8a912
josealexandre301428 <josealexandre301428@gmail.com>
2021-12-22 18:43:59 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ window.onload = function(){
       let captura = document.getElementById('texto-tarefa');
       let valor = captura.value;
       let row = document.createElement('li');
+      row.className = "itens";
       row.innerText = valor;
       lista.appendChild(row);
       captura.value = "";
     });
   }
 
+  //adiciona cor de funo ao item clicado de lista
+
+  function selectItem(){
+    let itens = document.getElementById('lista-tarefas');
+      itens.addEventListener('click', (event)=> {
+        /*let item = document.querySelectorAll(".itens");*/
+       
+          event.target.className = "fundoSelecao";
+        });
+      
+      }
+  selectItem();
   createList();
 }
